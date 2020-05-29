112e1db954595c35f82cafd3000ff5bfebc5fc6b
jeswin <shield178@gmail.com>
2020-05-29 10:34:20 +0800

    spa-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch , Route , Redirect } from 'react-router-dom';
@@ -11,19 +16,24 @@ class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
-
     
   render() {
     const HomePage = () => {
       return(
-          <Home 
-          />
-      );
+        <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+    );
     }
 
     
@@ -34,6 +44,7 @@ class Main extends Component {
         <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Redirect to="/home" />
+              <Route exact path='/contactus' component={Contact} />
         </Switch>
         <Footer />
       </div>
